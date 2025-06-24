95ce4ee4e08d1c35399d708a060a1ac745c741c5
Aleksanderzerokder <aleksanderzerokder@gmail.com>
2025-06-24 13:44:00 +0300

    Update models.py

--- schemas/models.py
+++ schemas/models.py
@@ -1,1 +1,14 @@
+# Файл: schemas/models.py
+from pydantic import BaseModel
+from typing import List, Literal
+from enum import Enum
 
+class Marketplace(str, Enum):
+    WILDERRIES = "wildberries"
+    OZON = "ozon"
+    YANDEX_MARKET = "yandex_market"
+
+class AnalysisRequest(BaseModel):
+    marketplace: Marketplace
+    period_days: int
+    sku_list: List[str] | Literal["all"]
